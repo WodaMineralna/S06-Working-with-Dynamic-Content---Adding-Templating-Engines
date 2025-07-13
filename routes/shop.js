4ce47448836a7285072c4933f1fa21cb5fdc9f9c
WodaMineralna <raknieborak65@gmail.com>
2025-07-13 19:38:22 +0200

    Installing, Implementing Pug & Outputting Dynamic HTML Content

--- routes/shop.js
+++ routes/shop.js
@@ -3,16 +3,25 @@ const path = require("path");
 const express = require("express");
 
 const rootDir = require("../util/path");
-const adminData = require("./admin")
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log('[shop.js] In "/" middleware!', 'Products data:' , adminData.products);
+  console.log(
+    '[shop.js] In "/" middleware!',
+    "Products data:",
+    adminData.products
+  );
 
-  // * Default Response Header: "text/html"
-  res.sendFile(path.join(rootDir, "views", "shop.html")); // ! We use path.join() so it works on Linux & Windows !!
-  // res.sendFile(path.resolve("views", "shop.html")); // * We can also do it like that
+  const products = adminData.products;
+
+  // ! We dont have to specify the templating engine / path to the 'views' folder, because we did it in app.js
+  res.render("shop", { products, docTitle: "Shop" });
+
+  // // * Default Response Header: "text/html"
+  // res.sendFile(path.join(rootDir, "views", "shop.html")); // // ! We use path.join() so it works on Linux & Windows !!
+  // // res.sendFile(path.resolve("views", "shop.html")); // * We can also do it like that
 });
 
 module.exports = router;
